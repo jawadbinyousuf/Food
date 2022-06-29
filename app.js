3c9dd882cf9aa969a070235a4b6087eb7d9b7e52
Md. Jawad Bin Yousuf <67420377+jawadbinyousuf@users.noreply.github.com>
2022-06-29 14:56:54 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const barBtn = document.querySelector('#bar-btn')
 const navBar = document.querySelector('.navbar')
 const closeBtn = document.querySelector('#close-btn')
+const fruitBtn = document.querySelectorAll('.btn3')
+const fruitBar = document.querySelectorAll('.fruit')
 
 barBtn.addEventListener('click', () =>{
     navBar.classList.toggle('active')
@@ -8,6 +10,10 @@ barBtn.addEventListener('click', () =>{
 closeBtn.addEventListener('click', () =>{
     navBar.classList.remove('active')
 })
+
+
+
+
 
 window.onscroll = () => {
     navBar.classList.remove('active')
